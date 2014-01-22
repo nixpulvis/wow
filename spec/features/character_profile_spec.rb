7d7c44493c68e2fb3ab4264a6f83a0e56e8176e0
Nathan Lilienthal <nathan@nixpulvis.com>
2014-01-22 11:01:54 -0500

    Starting testing, this shit is important.

---
+++ spec/features/character_profile_spec.rb
@@ -0,0 +1,50 @@
+require "spec_helper"
+
+describe WoW::CharacterProfile do
+  subject { WoW::CharacterProfile.new("Stormreaver", "Epicgrim") }
+
+  describe "#initialize" do
+    it "is valid server, and name" do
+      expect {
+        subject
+      }.not_to raise_error
+    end
+
+    it "is valid with special characters in name" do
+      expect {
+        WoW::CharacterProfile.new("Stormreaver", "Disciplíne")
+      }.not_to raise_error
+    end
+
+    it "is invalid server" do
+      expect {
+        WoW::CharacterProfile.new("Notaserver", "Epicgrim")
+      }.to raise_error
+    end
+
+    it "is invalid name" do
+      expect {
+        WoW::CharacterProfile.new("Stormreaver", "Imnotaguyatalllolplz")
+      }.to raise_error
+    end
+
+    it "is invalid server and name" do
+      expect {
+        WoW::CharacterProfile.new("Notaserver", "Imnotaguyatalllolplz")
+      }.to raise_error
+    end
+  end
+
+  describe "#lookup" do
+    it "looks up class" do
+      subject.lookup(:class).should eq(:druid)
+    end
+  end
+
+  describe "#[]" do
+    it "acts as a hash" do
+      subject[:name].should eq("Epicgrim")
+    end
+  end
+
+end
